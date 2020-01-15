25cbb8df644aaabd86d49f3f84d74642336ee4a0
Jonathan Rosal <jonathanp@rosal.us>
2020-01-14 16:42:16 -0800

    IM AFRAID OF COMMITMENT

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,20 @@
 //
 // Created by aknight on 8/2/19.
 //
+/**
+ * CS V30 Beginning C++
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @valladares.jose805@gmail.com
+ */
 
+#include <iostream>
+#include <csv30>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
